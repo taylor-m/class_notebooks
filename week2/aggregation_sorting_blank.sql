160d470b56c5031eec129fa8f2e9547fb257b06e
taylor-m <53980659+taylor-m@users.noreply.github.com>
2020-11-07 15:07:42 -0600

    Create aggregation_sorting_blank.sql

---
+++ week2/aggregation_sorting_blank.sql
@@ -0,0 +1,57 @@
+-- Using table: fueleconomy.public.vehicles
+-- No context for these prompts :(  Straight to business.
+
+-- How many records are in the vehicles table?
+
+
+
+-- How many records are there that use Diesel fuel?
+
+
+
+-- How many non-NULL records are there in the cyl column?
+
+
+
+-- Can you explain why the cyl column has NULLs?
+
+
+
+-- List the unique fuel types when cyl is NULL
+
+
+
+-- Count the unique number of makes when cyl is NULL
+
+
+
+-- What is the average hwy mpg? cty mpg?
+
+
+
+-- What brands make the most fuel efficient cars?
+-- Include a count of the number of records for each make to get
+-- additional perspective?
+
+
+
+-- Redo the last analysis but with a minimum number of records for
+-- a make to be elligible.  Let's say 1000 to be considered.
+
+
+
+-- Redo the last analysis but only consider Regular fuel.
+-- So our question were answering now.  Which large* makes produce
+-- the most fuel efficient cars (where large is >1000 records)?
+
+
+
+-- What are the top 5 most fuel efficient cars that
+-- have any type of four-wheel or all-wheel drive
+
+
+
+-- Create a report that shows the worst & best hwy mpg before 1990
+-- and after 1990
+
+
